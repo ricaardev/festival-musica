b48ffca09ccf7148c3b02711242c35fb41732ebf
ricaardev <ricaar.dev@gmail.com>
2024-04-30 18:25:56 +0200

    end footer

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ const sass = gulpSass(dartSass)
 
 export function css(done) {
 
-    src('src/scss/app.scss') //ubicamos el archivo 
+    src('src/scss/app.scss', { sourcemaps: true }) //ubicamos el archivo y añadimos sourcemaps: true para que cree el archivo app.css.map
         .pipe(sass().on('error', sass.logError)) //aplicamos sass pero da un error, por eso le aplicamo un listner .on que loguea el error en la terminal
-        .pipe(dest('build/css')) // este es el destino del compilado
+        .pipe(dest('build/css', { sourcemaps: true })) // este es el destino del compilado
 
     done()
 }
